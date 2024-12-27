d5d98c5ebebdd36ca8b3b503cf55c1a603c8de84
Syed Muntazir Mehdi <mehdis2@uni.coventry.ac.uk>
2024-12-27 16:02:24 +0500

    Time: 266 ms (69.61%), Space: 18 MB (21.96%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -4,16 +4,16 @@ class Solution:
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 left -= 1
                 right += 1
-            return s[left + 1:right]  # Return the palindrome substring
+            return s[left + 1:right]  
 
         longest = ""
         for i in range(len(s)):
-            # Odd-length palindrome (single character center)
+            
             pal1 = expand_around_center(i, i)
-            # Even-length palindrome (two character center)
+           
             pal2 = expand_around_center(i, i + 1)
             
-            # Update longest palindrome
+            
             if len(pal1) > len(longest):
                 longest = pal1
             if len(pal2) > len(longest):
